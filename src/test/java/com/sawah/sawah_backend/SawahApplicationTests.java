3e448233ac91b484773275d6c9570a6e4e936b44
omar samir <omersamer120@gmail.com>
2026-02-22 21:21:48 +0200

    Initial commit for Sawah project structure and dependencies

---
+++ src/test/java/com/sawah/sawah_backend/SawahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawah.sawah_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SawahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
