1a70d10ef22ea89303d691c84ea11e0ca3649b94
omar samir <omersamer120@gmail.com>
2026-03-25 23:00:12 +0200

    refactor: update (CustomUserDetailsService) add UserService and delete UserRepository

--- src/main/java/com/sawah/sawah_backend/security/user/CustomUserDetailsService.java
+++ src/main/java/com/sawah/sawah_backend/security/user/CustomUserDetailsService.java
@@ -2,6 +2,8 @@ package com.sawah.sawah_backend.security.user;
 
 import com.sawah.sawah_backend.models.User;
 import com.sawah.sawah_backend.repository.UserRepository;
+import com.sawah.sawah_backend.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,16 +11,17 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmailWithRoles(username).orElseThrow(()
-                -> new UsernameNotFoundException("User Not Found"));
+    public UserDetails loadUserByUsername(String username){
+
+        User user = userService.findUserByEmailWithRoles(username);
 
        return new CustomUserDetails(user);
+
     }
 }
