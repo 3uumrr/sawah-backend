f2ac57f18ef15ebb547d1dfc0b4573045c22a540
omar samir <omersamer120@gmail.com>
2026-03-30 23:40:28 +0200

    refactor: update (EmailServiceImpl) enable asynchronous email sending and refine error handling

--- src/main/java/com/sawah/sawah_backend/service/email/EmailServiceImpl.java
+++ src/main/java/com/sawah/sawah_backend/service/email/EmailServiceImpl.java
@@ -1,9 +1,11 @@
 package com.sawah.sawah_backend.service.email;
 
+import com.sawah.sawah_backend.exceptions.EmailDeliveryException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
@@ -16,6 +18,7 @@ public class EmailServiceImpl implements EmailService{
     private final MessageSource messageSource;
 
     @Override
+    @Async
     public void sendVerificationCode(String to, String code, Locale locale) {
         String subject = messageSource.getMessage("mail.reset.subject", null, "Password Reset", locale);
         String text = messageSource.getMessage("mail.reset.body", new Object[]{code}, "Your code is: " + code, locale);
@@ -28,7 +31,7 @@ public class EmailServiceImpl implements EmailService{
         try {
             mailSender.send(message);
         } catch (Exception e) {
-            throw new RuntimeException("Failed to send email: " + e.getMessage());
+            throw new EmailDeliveryException("error.mail.send.failed");
         }
     }
 
