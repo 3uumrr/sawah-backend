99462b377a96d0d55c416d942fcf0b4c2d75cf21
omar samir <omersamer120@gmail.com>
2026-09-14 08:24:18 +0300

    refactor(vehicle): enrich VehicleType with default passenger capacities
    Embedded passenger limits directly into vehicle definitions for ride allocation logic.

--- src/main/java/com/sawah/sawah_backend/enums/VehicleType.java
+++ src/main/java/com/sawah/sawah_backend/enums/VehicleType.java
@@ -1,8 +1,18 @@
 package com.sawah.sawah_backend.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum VehicleType {
-    SEDAN,
-    SUV,
-    VAN,
-    MICROBUS
+    SEDAN(4),
+    SUV(6),
+    VAN(8),
+    MICROBUS(12);
+
+    private final int capacity;
+
+    VehicleType(int capacity) {
+        this.capacity = capacity;
+    }
+
 }
