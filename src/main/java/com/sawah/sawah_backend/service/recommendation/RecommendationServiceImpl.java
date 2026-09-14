7155ff3e68bd6144cef17a183536203808f2a098
omar samir <omersamer120@gmail.com>
2026-09-14 08:39:41 +0300

    refactor(recommendation): optimize and clean up unused imports in RecommendationServiceImpl

--- src/main/java/com/sawah/sawah_backend/service/recommendation/RecommendationServiceImpl.java
+++ src/main/java/com/sawah/sawah_backend/service/recommendation/RecommendationServiceImpl.java
@@ -2,13 +2,11 @@ package com.sawah.sawah_backend.service.recommendation;
 
 import com.sawah.sawah_backend.dto.recommendation.RecommendationRequestDto;
 import com.sawah.sawah_backend.dto.recommendation.RecommendationResponseDto;
-import com.sawah.sawah_backend.models.Place;
 import com.sawah.sawah_backend.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import java.util.List;
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
